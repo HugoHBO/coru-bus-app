11acd115984d9ac9526c380d3c538d32ac494e5b
Hugo <akikamuy@gmail.com>
2025-05-30 01:03:38 +0200

    favoritos, creación de reloj y ajustes varios

--- coruBusFront/src/app/components/shared/navbar/navbar.component.ts
+++ coruBusFront/src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,15 +8,37 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  horaActual: string = '';
+  private intervalo!: any;
 
   constructor(private _router: Router) {}
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+ ngOnInit(): void {
+  this.actualizarHora();
+  const ahora = new Date();
+  const msHastaSiguienteMinuto = (60 - ahora.getSeconds()) * 1000;
+
+  setTimeout(() => {
+    this.actualizarHora();
+    this.intervalo = setInterval(() => this.actualizarHora(), 60000);
+  }, msHastaSiguienteMinuto);
+}
+  actualizarHora(): void {
+    const ahora = new Date();
+    const horas = ahora.getHours().toString().padStart(2, '0');
+    const minutos = ahora.getMinutes().toString().padStart(2, '0');
+    this.horaActual = ` ${horas} : ${minutos}`;
+  }
 
   irAFavoritos() {
-    this._router.navigate([])
+    this._router.navigate([]);
   }
 
   irALineas() {
-    this._router.navigate(["/"])
+    this._router.navigate(['/']);
   }
 }
